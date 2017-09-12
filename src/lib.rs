244a739ac95e7808a11e59ab1cc04032f3d56bb1
p_2 <30770254+drmagsipoc@users.noreply.github.com>
2017-09-12 14:59:09 +0800

    1. Moved html files to bin. 2. Added thread execution in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,25 @@
 use std::thread;
-struct Worker {
-    id: usize,
-    thread: thread::JoinHandle<()>,
-}
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 
-impl Worker {
-    pub fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
-
-        Worker {
-            id,
-            thread,
-        }
-    }
-}
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
+type Job = Box<FnBox + Send + 'static>;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -31,15 +32,20 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
             // create some threads and store them in a vector
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, receiver.clone()));
         }
 
         ThreadPool {
-            workers
+            workers,
+            sender,
         }
     }
 
@@ -47,6 +53,32 @@ impl ThreadPool {
         where
             F: FnOnce() + Send + 'static
     {
+        let job = Box::new(f);
 
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+
+                println!("Worker {} got a job; executing.", id);
+                
+                job.call_box();
+            }
+        });
+
+        Worker {
+            id,
+            thread,
+        }
     }
 }
